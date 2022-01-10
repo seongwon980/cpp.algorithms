1c2d0aa6867b91ebfc148aada5c3ff34f01861a6
seongwon980 <poiuy98749@yonsei.ac.kr>
2022-01-10 19:28:03 +0900

    0110

--- Book_Examples/04-3.cpp
+++ Book_Examples/04-3.cpp
@@ -2,6 +2,21 @@
 using namespace std;
 
 int main(void) {
-    cout << "Hello" << endl;
+    int num1, num2, num3, result;
+    int arr[10] = {0,};
+
+    cin >> num1 >> num2 >> num3;
+    result = num1 * num2 * num3;
+
+    while (result > 0) {
+        arr[result % 10]++;
+        result = result / 10;
+    }
+
+    for (int i = 0; i < 10; i++) {
+        cout << arr[i] << endl;
+    }
+
     return 0;
 }
+
