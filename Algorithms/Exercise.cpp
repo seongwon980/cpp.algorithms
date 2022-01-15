0553bd2cf1496497527d0ae417be84f20a7a08e2
seongwon980 <poiuy98749@yonsei.ac.kr>
2022-01-15 16:36:01 +0900

    qwe

--- Algorithms/Exercise.cpp
+++ Algorithms/Exercise.cpp
@@ -2,28 +2,44 @@
 #include <cstring>
 using namespace std;
 
+
+bool IsGroupWord(char* str, int size) {
+    bool alphabets[26] = {false,};
+    int currentIndex = 0;
+    while (currentIndex < size) {
+        char currentChar = str[currentIndex];
+        alphabets[currentChar - 'a'] = true;        // mark as explored
+        
+        while (str[currentIndex] == currentChar) currentIndex++;     // skip same letters
+        if (currentIndex >= size) return true;      // Reached the end of string
+
+        // Found a new letter
+        currentChar = str[currentIndex];
+        if (alphabets[currentChar - 'a'])       // Already explored -> not a group
+            return false;
+    }
+
+    return true;
+}
 int main(void) {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
+    int num_of_tc;
+    cin >> num_of_tc;
     char input_str[101];
-    cin >> input_str;
-    int size = strlen(input_str);
+    int size;
     int count = 0;
 
-    for (int i = 0; i < size; i++) {
-        count += 1;
-        if (input_str[i] == 'c') {
-            // 'c=' or 'c-'
-            if (input_str[i + 1] == '=' || input_str[i + 1] == '-')
-                i += 1;
-        } else if (input_str[i] == 'd') {
-            // 'dz=' or 'd-'
-            if (i <= size - 2 && input_str[i + 1] == 'z' && input_str[i + 2] == '=')
-                i += 2;
-            else if (i <= size - 1 && input_str[i + 1] == '-')
-                i += 1 ;
-        }  else if (input_str[i])
+    for (int i = 0; i < num_of_tc; i++) {
+        cin >> input_str;
+        size = strlen(input_str);
+        if (IsGroupWord(input_str, size))
+            count += 1;
     }
+
+    cout << count << endl;
+    return 0;
 }
+
