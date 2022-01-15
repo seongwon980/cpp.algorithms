02c26eef64de4ed7e582f8cc4c126a6f34eb2ca0
seongwon980 <poiuy98749@yonsei.ac.kr>
2022-01-15 15:31:03 +0900

    2022-01-15 15:30

--- Algorithms/Exercise.cpp
+++ Algorithms/Exercise.cpp
@@ -7,17 +7,23 @@ int main(void) {
     cin.tie(NULL);
     cout.tie(NULL);
 
-    int alphabets[26] = {3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6,
-    7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 10, 10, 10, 10};
-    
-    char input_str[16];
+    char input_str[101];
     cin >> input_str;
+    int size = strlen(input_str);
     int count = 0;
 
-    for (int i = 0; i < strlen(input_str); i++) {
-        count += alphabets[input_str[i] - 'A'];
+    for (int i = 0; i < size; i++) {
+        count += 1;
+        if (input_str[i] == 'c') {
+            // 'c=' or 'c-'
+            if (input_str[i + 1] == '=' || input_str[i + 1] == '-')
+                i += 1;
+        } else if (input_str[i] == 'd') {
+            // 'dz=' or 'd-'
+            if (i <= size - 2 && input_str[i + 1] == 'z' && input_str[i + 2] == '=')
+                i += 2;
+            else if (i <= size - 1 && input_str[i + 1] == '-')
+                i += 1;
+        }  else if (input_str[i])
     }
-
-    cout << count;
-
 }
